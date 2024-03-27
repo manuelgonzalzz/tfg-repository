3e327831eedfa5c5255ad7483532fa8f79d9687c
manuelgonzalez <manuelgonzalezcanizares@gmail.com>
2024-03-27 17:14:36 +0100

    graficas-finales

--- code/simulacion.py
+++ code/simulacion.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import scipy 
 import ROOT as root
+import scipy as sp
 
 # Funcion Uniformidad Integral.
 # Input: N_pix, C_med.
@@ -160,3 +160,223 @@ def histogram_name(N_pix,nct,filtro,t=0,k=0):
         k= str(int(k*10))
     name = N_pix +'-'+nct+ '-f' + filtro + '-' + t + '-' + k + ".root"
     return name
+
+
+'''
+Funciones para el ANALISIS ESTADISTICO, incluyendo::
+-- Integracion
+-- Generar curvas ROC, dibujar curvas ROC
+-- Generar curvas area contraste, dibujar curvas area contraste
+-- Generar curvas contraste detalle, dibujar curvas contraste detalle
+'''
+
+#INTEGRAR 
+
+
+
+
+def trapecio(x,y):
+    integral = 0
+    for i in range(len(x)-1):
+        delta_x = x[i+1]-x[i]
+        integral = integral + delta_x*y[i] + 0.5*(y[i+1]-y[i])*delta_x
+    return abs(integral)
+
+def sorteo_montecarlo(x,y,xe,ye):
+    #Inicializo los nuevos puntos x,y a sortear
+    x_mc = np.zeros(len(x))
+    y_mc = np.zeros(len(x))
+    for i in range(len(x)):
+        x_mc[i]=np.random.normal(x[i],xe[i])
+        y_mc[i]=np.random.normal(y[i],ye[i])
+    #Uso x_errors, y_errors inicializados a cero porque no influyen
+    return x_mc,y_mc
+
+
+
+
+def montecarlo_trapecio(x,y,xe,ye):
+    integral_inicial = trapecio(x,y)
+    integral = np.array([])
+    for i in range(1000):
+        x_mc,y_mc=sorteo_montecarlo(x,y,xe,ye)
+        integral = np.append(integral,trapecio(x_mc,y_mc)) #Da igual que valores de xe,ye tomememos. De hecho creo que lo voy a borrar
+    integral = np.sort(integral)
+
+    '''for i in range(len(integral)-1):
+        if integral[i]<integral_inicial<integral[i+1]:
+            pos_inf = i
+            pos_sup = i+1
+    delta =  int(0.3413447460685429*len(integral))
+    
+    sigma_plus = integral[pos_sup+delta] - integral[pos_sup]
+    sigma_minus = integral[pos_inf] - integral[pos_inf-delta] 
+    sigma = 0.5*(sigma_minus + sigma_plus)      '''
+    sigma = np.std(integral)
+    return integral_inicial,sigma
+
+
+#--------------------CURVAS ROC y AREA CONTRASTE------------------------
+''' Voy a crear dos funciones principales:
+1. curvaroc(n_pix,nct,t,k)
+Input: modo de adquisicion
+Output: Datos(x,y,xe,ye) y gráfica curva roc
+
+2. areacontraste(n_pix,nct,t)
+Input: modo de aquisicion salvo k (ahora es una variable)
+Output: Objeto TGraphErrors y gráfica curva area contraste
+---------de momento no realizo en ajuste dentro de esta funcion.
+
+Tambien he incluido las siguientes funciones secundarias:
+3. ajuste_ac(x,y,xe,ye)
+Input: valores de la curva area-contraste
+Output: parametros a,b del ajuste a la curva logistica
+4. dibujarroc(x,y,xe,ye)
+5. dibujarAC(x,y,xe,ye) ---- sirven para dibujar y guardar en un archivo las curvas roc y area-contraste
+'''
+
+def curvaroc(N_pix,nct,t,k,dibujar=False): #de momento el filtro es f1
+
+    file_path = "./histogramas/64x64/"+str(nct)+"mc/"
+    name_sana = histogram_name(N_pix,nct,1)
+    name_defec = histogram_name(N_pix,nct,1,t,k)
+    file_sana = root.TFile(file_path+name_sana)
+    file_defec = root.TFile(file_path+name_defec)
+    hist_sana = file_sana.Get(name_sana)
+    hist_defec = file_defec.Get(name_defec)
+    sana = hist_sana.GetCumulative()
+    defec = hist_defec.GetCumulative()
+    nbins = hist_sana.GetNbinsX() #nbins==400 Se debería aumentar?
+    x,y = np.zeros(400),np.zeros(400)
+    xe,ye = incertidumbre(sana,10000),incertidumbre(defec,10000)
+    for i in range(400):
+        y[i] = 1-defec.GetBinContent(i)
+        x[i] = 1-sana.GetBinContent(i)
+    if dibujar==True: dibujarroc(x,y,xe,ye,root.kBlue)
+    #se podria plantear la opcion de devolver un .cv con los datos
+    return x,y,xe,ye
+
+def areacontraste(N_pix,nct,t,dibujar=False): #k no es input, es una variable de output
+    k_list=[0,0.5,1,1.5,2,2.5,3,3.5,4,4.5,5,5.5,6,6.5,7,7.5,8,8.5,9,9.5,10]
+    integral_list=[]
+    error_list=[]
+    for k in k_list:
+        x,y,xe,ye = curvaroc(N_pix,nct,t,k)
+        integral,error = montecarlo_trapecio(x,y,xe,ye)
+        integral_list.append(integral)
+        error_list.append(error)
+    x = np.array(k_list)
+    xe = np.zeros(len(x))
+    y = np.array(integral_list)
+    ye = np.array(error_list)
+    #dibujarAC(x,y,xe,ye,root.kRed)
+    return x,y,xe,ye
+    
+def ajuste_ac(x,y):
+    #1. Transformar los datos para realizar un ajuste lineal.
+    x_fit,y_fit = np.zeros(len(x)),np.zeros(len(x))
+    for i in range(len(x)):
+        if 0.51<y[i]<0.99: #No consideros los puntos en los extremos saturados
+            x_fit[i] = x[i] #x no cambia
+            y_fit[i] = np.log(1/(2*y[i]-1)-1) #funcion de ajuste invertida -- falta considerar la transformacion de los errores
+    x_fit = x_fit[x_fit!=0] #Elimino los elementos no nulos
+    y_fit = y_fit[y_fit!=0]
+    x_min = np.min(x_fit)
+    x_max = np.max(x_fit)
+    curvalineal = root.TGraph(len(x_fit),x_fit,y_fit)
+    linearfit = root.TF1("linearfit","1++x",x_min,x_max) # a+b*x
+    curvalineal.Fit(linearfit,"C")
+    linearfit = curvalineal.GetFunction("linearfit")
+    a = linearfit.GetParameter(0)
+    b = linearfit.GetParameter(1)
+    
+
+    return a,b
+    
+  
+def dibujarroc(x,y,xe,ye,color): #los argumentos son los valores de fvn y ffn; color es un elemento kcolor
+    roc = root.TGraphErrors(400)
+    for i in range(400):
+        roc.SetPoint(i,x[i],y[i])
+        roc.SetPointError(i,xe[i],ye[i])
+    roc.SetTitle("Receiver Operating Characteristic (ROC)")
+    roc.GetXaxis().SetTitle("False Positive Rate")
+    roc.GetYaxis().SetTitle("True Positive Rate")
+    canvas = root.TCanvas('','',800,600)   
+    roc.SetMarkerColor(color)
+    roc.SetLineColor(color)
+    roc.SetMarkerStyle(24)
+    roc.Draw("APX") #los errores de momento son menores al tamaño de Marker
+    canvas.SaveAs("roc_plot.png")
+
+def dibujarAC(x,y,xe,ye,color): #los argumentos son los valores de fvn y ffn; color es un elemento kcolor
+    roc = root.TGraphErrors(len(x))
+    for i in range(len(x)):
+        roc.SetPoint(i,x[i],y[i])
+        roc.SetPointError(i,xe[i],ye[i])
+    ajuste = root.TGraph(10000)
+    x_fit = np.zeros(10000)
+    y_fit = np.zeros(10000)
+    a,b = ajuste_ac(x,y)
+    for i in range(10000):
+        x_fit[i] = i*10/10000
+        y_fit[i] = 0.5*(1+1/(1+np.exp(a+b*x_fit[i])))
+        ajuste.SetPoint(i,x_fit[i],y_fit[i])
+    ajuste.SetLineColor(color-5)
+    roc.SetTitle("Curva AC: Area-Contraste")
+    roc.GetXaxis().SetTitle("k - contraste")
+    roc.GetYaxis().SetTitle("Aroc - Area curva roc")
+    canvas = root.TCanvas('','',800,600)   
+    roc.SetMarkerColor(color)
+    roc.SetLineColor(color)
+    roc.SetMarkerStyle(24)
+    roc.Draw("AP") #los errores de momento son menores al tamaño del Marker
+    ajuste.Draw("SAME")
+    canvas.SaveAs("ac_plot.png")
+
+#-------------------CURVAS CONTRASTE DETALLE-------------------
+def contrastedetalle(Npix,nct):
+    t_list = np.array([1,2,3,4,5])
+    k_list = np.zeros(len(t_list))
+    sigma_list = np.zeros(len(t_list))
+    for i in range(len(t_list)):
+        x,y,xe,ye = areacontraste(Npix,nct,t_list[i],False)
+        a,b=ajuste_ac(x,y)
+        aroc = 0.75 #Esto puede ser modificado a 0.8 -- 0.9
+        if -0.01<b<0.01: break
+        k = (1/b) * (-a + np.log(1/(2*aroc-1)-1))
+        #implemento el montecarlo directamente
+        montecarlo=np.zeros(1000)
+        '''for j in range(1000):
+            a_mc = np.random.normal(a,asigma)
+            b_mc = np.random.normal(b,bsigma)
+            if abs(b_mc)>0.01:
+                k_mc = (1/b_mc)* (-a_mc + np.log(1/(2*aroc-1)-1))
+            montecarlo[j]=k_mc'''
+        for j in range(1000):
+            ymc = np.zeros(len(x))
+            for l in range(len(x)):
+                ymc[l]=np.random.normal(y[l],ye[l])
+            amc,bmc=ajuste_ac(x,ymc)
+            if abs(bmc)>0.01:
+                kmc = (1/bmc)*(-amc + np.log(1/(2*aroc-1)-1))
+            montecarlo[j] = kmc
+        k_list[i] = k
+        sigma_list[i] = np.std(montecarlo)
+    return t_list**2,k_list,sigma_list
+
+
+def dibujarCD(x,y,xe,ye,color,nct): #los argumentos son los valores de fvn y ffn; color es un elemento kcolor
+    roc = root.TGraphErrors(len(x))
+    for i in range(len(x)):
+        roc.SetPoint(i,x[i],y[i])
+        roc.SetPointError(i,xe[i],ye[i])
+    roc.SetTitle("Curva CD: Contraste-Detalle")
+    roc.GetXaxis().SetTitle("t^{2} - tamano del defecto (detalle)")
+    roc.GetYaxis().SetTitle("k_50 - contraste")
+    canvas = root.TCanvas('','',800,600)   
+    roc.SetMarkerColor(color)
+    roc.SetLineColor(color)
+    roc.SetMarkerStyle(24)
+    roc.Draw("AP") #los errores de momento son menores al tamaño del Marker
+    canvas.SaveAs("cd-"+str(nct)+".png")
